3dd819d0996ba998fbcef269eadeba6b8569dc1e
Vimal_G <55605963+slyflare@users.noreply.github.com>
2021-02-10 18:47:47 -0500

    updated loops

--- src/StoreManager.java
+++ src/StoreManager.java
@@ -4,31 +4,33 @@
 // Coded by Vimal. Reviewed and refactored by Nikita //
 
 public class StoreManager {
-    private Inventory inventory;
+        private Inventory inventory;
 
-    public StoreManager(Inventory i) {
-        this.inventory = i;
-    }
+        public StoreManager(Inventory i) {
+            this.inventory = i;
+        }
 
-    public int checkStock(int id) {
-        return inventory.getStock(id);
-    }
+        public int checkStock(String name) {
+            int id = 0;
+            for (Product p : inventory.getProductList()) {
+                if (p.getItemName().equals(name)) {
+                    id = p.getItemID();
+                }
+            }
+            return inventory.getStock(id);
+        }
 
-    public String checkOut(int[][] cart) {
-        double totalPrice = 0;
-        for (int[] c : cart) {
-            for (int[] i : inventory.getArrayStock()) {
-                if (c[0] == i[0]) {
-                    assert (c[1] < i[1]) : "Not enough " + inventory.getProductInfo(i[0]) +" in stock";
-                    inventory.sellStock(c[0], c[1]);
-                    for (Product p : inventory.getProductList()) {
-                        if (c[0] == p.getItemID()) {
-                            totalPrice += p.getItemCost() * c[1];
-                            }
-                        }
+        public String checkOut(int[][] cart) {
+            double totalPrice = 0;
+            for (int[] c : cart) {
+                for (Product p1 : inventory.getProductList()) {
+                    if (c[0] == p1.getItemID()) {
+                        assert (c[1] < p1.getItemStock()) : "Not enough " + inventory.getProductInfo(p1.getItemID()) + " in stock";
+                        inventory.sellStock(c[0], c[1]);
+                        totalPrice += p1.getItemCost() * c[1];
                     }
                 }
             }
-        return "Total price: $" + totalPrice;
+            return "Total price: $" + totalPrice;
+        }
     }
-}
