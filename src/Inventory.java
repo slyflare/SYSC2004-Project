a65dd4f56dac8bec9350f3ca6d18a967969ba6ba
Vimal_G <55605963+slyflare@users.noreply.github.com>
2021-02-10 18:46:17 -0500

    removed 2d array

--- src/Inventory.java
+++ src/Inventory.java
@@ -6,19 +6,18 @@
 import java.util.ArrayList;
 
 public class Inventory {
-    private int[][] stock;
     private ArrayList<Product> productList = new ArrayList<>();
 
-    public Inventory(int[][] baseInventory, ArrayList<Product> list){
-        this.stock = baseInventory;
+    public Inventory(ArrayList<Product> list){
         this.productList = list;
     }
 
     //Getters
+
     public int getStock(int id){
-        for(int[] i: stock){
-            if(i[0] == id){
-                return i[1];
+        for(Product p: productList){
+            if(p.getItemID() == id){
+                return p.getItemStock();
             }
         }
         System.out.println("Item does not exist in inventory");
@@ -35,10 +34,6 @@ public class Inventory {
     }
 
     /**Might not be used*/
-    public int[][] getArrayStock() {
-        return stock;
-    }
-    /**Might not be used*/
     public ArrayList<Product> getProductList() {
         return productList;
     }
@@ -46,32 +41,25 @@ public class Inventory {
     //Setters
 
     public void addStock(int id, int amount){
-        for(int[] i: stock){
-            if(i[0] == id){
-                i[1] += amount;
+        for(Product p: productList){
+            if(p.getItemID() == id){
+                p.setItemStock(p.getItemStock()+amount);
             }
         }
     }
 
     public void sellStock(int id, int amount){
-        for(int[] i: stock){
-            if(i[0] == id){
-                i[1] -= amount;
+        for(Product p: productList){
+            if(p.getItemID() == id){
+                p.setItemStock(p.getItemStock()-amount);
             }
         }
     }
 
-    /**Might not be used*/
-    public void setStock(int[][] stock) {
-        this.stock = stock;
-    }
-
-    /**Might not be used*/
     public void addProduct(Product p){
         this.productList.add(p);
     }
-    
-    /**Might not be used*/
+
     public void removeProduct(Product p){
         this.productList.remove(p);
     }
