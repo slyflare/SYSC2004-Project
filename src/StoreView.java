87ac2152556a1c596d0d132f59609ab7b224314e
Vimal_G <55605963+slyflare@users.noreply.github.com>
2021-03-03 22:34:44 -0500

    added scanner example & user list

--- src/StoreView.java
+++ src/StoreView.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StoreView {
 
     private ShoppingCart cart;
@@ -20,12 +22,18 @@ public class StoreView {
         StoreView sv1 = new StoreView(cart1);
         StoreView sv2 = new StoreView(cart2);
         StoreView sv3 = new StoreView(cart3);
+        StoreView[] users = {sv1,sv2,sv3};
 
         System.out.println("test");
         System.out.println(sv1.getCart().getCartID());
         System.out.println(sv2.getCart().getCartID());
         System.out.println(sv3.getCart().getCartID());
 
+        //scanner example
+        Scanner s = new Scanner(System.in);
+        System.out.println("Name?");
+        String name = s.nextLine();
+        System.out.println("Name is " + name);
     }
 }
 
