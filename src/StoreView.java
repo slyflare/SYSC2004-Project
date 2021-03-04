1041e36f25fbd887b26b2a71758f239d9d28d265
Vimal_G <55605963+slyflare@users.noreply.github.com>
2021-03-03 22:20:48 -0500

    StoreView first iteration

---
+++ src/StoreView.java
@@ -0,0 +1,33 @@
+public class StoreView {
+
+    private ShoppingCart cart;
+
+    public StoreView(ShoppingCart cart){
+        this.cart = cart;
+    }
+
+    //delet_this
+    public ShoppingCart getCart() {
+        return cart;
+    }
+
+    public static void main(String[] args) {
+
+        StoreManager sm = new StoreManager();
+        ShoppingCart cart1 = sm.initialiseCart();
+        ShoppingCart cart2 = sm.initialiseCart();
+        ShoppingCart cart3 = sm.initialiseCart();
+        StoreView sv1 = new StoreView(cart1);
+        StoreView sv2 = new StoreView(cart2);
+        StoreView sv3 = new StoreView(cart3);
+
+        System.out.println("test");
+        System.out.println(sv1.getCart().getCartID());
+        System.out.println(sv2.getCart().getCartID());
+        System.out.println(sv3.getCart().getCartID());
+
+    }
+}
+
+
+
