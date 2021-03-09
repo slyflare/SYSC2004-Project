8c5e480d4586ef1cc4887891abbfd94f52e99e89
Vimal_G <55605963+slyflare@users.noreply.github.com>
2021-03-08 19:02:10 -0500

    javadocs

--- src/Milestone2/ShoppingCart.java
+++ src/Milestone2/ShoppingCart.java
@@ -1,25 +1,46 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+*ShoppingCart subclass of Inventory
+*/
+
 public class ShoppingCart extends Inventory{
 
     private int cartID;
     private HashMap<Product, Integer> cartItems;
 
+    /**
+    *Default constructor for ShoppingCart
+    */
     public ShoppingCart(int cartID){
         this.cartID = cartID;
         this.cartItems = new HashMap<>();
     }
 
     //Getters
+    
+    /**
+    *Gets cart id
+    *@return int, the cart id 
+    */
     public int getCartID() {
         return cartID;
     }
 
+    /**
+    *Returns an arraylist of all the products in the cart
+    *@return ArrayList<Product> all the products in the cart
+    */
     public ArrayList<Product> getItems(){
         return new ArrayList<>(cartItems.keySet());
     }
 
+    /**
+    *Gets the amount of a certain product in the cart
+    *@param pID int, the product id
+    *@return int, the amount of that product in the cart
+    */
     public int getCartAmount(int pID){
         for(Product p: cartItems.keySet()){
             if(p.getItemID() == pID){
@@ -29,6 +50,10 @@ public class ShoppingCart extends Inventory{
         return 0;
     }
 
+    /**
+    *Gets all the cart products along with their id and amount
+    *@return String, cart info
+    */
     public String getCartInfo(){
         StringBuilder s = new StringBuilder();
         for(Product p : this.cartItems.keySet()){
@@ -38,12 +63,22 @@ public class ShoppingCart extends Inventory{
     }
 
     //Setters
+    
+    /**
+    * Sets the cart's id
+    *@param newID int, the new cart id
+    */
     public void setCartID(int newID){
         this.cartID = newID;
     }
 
     //Other Methods
 
+    /**
+    *Adds product and its amount to cart
+    *@param amount, int the amount of product to be added
+    *@param ID int, the id of the specified product
+    */
     public void addToCart(int amount, int ID){
             if (getProduct(ID).getItemID() != 0) {
                 if(cartItems.get(getProduct(ID)) != null){
@@ -58,6 +93,11 @@ public class ShoppingCart extends Inventory{
             }
     }
 
+    /**
+    *Removes product and its amount to cart
+    *@param amount, int the amount of product to be removed
+    *@param ID int, the id of the specified product
+    */
     public void removeFromCart(int amount, int ID){
         for(Product p: this.cartItems.keySet()){
             if (p.getItemID() == ID){
@@ -74,6 +114,9 @@ public class ShoppingCart extends Inventory{
 
     }
 
+    /**
+    *Clears all the contents of cart
+    */
     public void clearCart(){
         for(Product p: this.cartItems.keySet()){
             this.cartItems.remove(p);
