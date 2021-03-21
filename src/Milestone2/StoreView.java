e5bd2520b01f9962fdfdcd2a632b6b3224fe27a8
InexactCactus6 <78772034+InexactCactus6@users.noreply.github.com>
2021-03-21 01:23:22 -0400

    Update StoreView.java

--- src/Milestone2/StoreView.java
+++ src/Milestone2/StoreView.java
@@ -12,18 +12,38 @@ public class StoreView {
     private StoreManager storeMan;
 
     /**
-    *Default StoreView constructor
-    */
+     *Default StoreView constructor
+     */
     public StoreView(StoreManager storeMan) {
         this.storeMan = storeMan;
         this.cart = storeMan.newCart();
     }
 
+    /**
+     * Input verification exception handling method that ensures user input is a valid integer.
+     * @return Returns user input in the form of an int.
+     */
+    public int verifyNumInput(){
+        boolean correctInput = false;
+        int output = 0;
+        while(!correctInput) {
+            try {
+                Scanner newItem = new Scanner(System.in);
+
+                output = Integer.parseInt(newItem.nextLine());
+                correctInput = true;
+            } catch (NumberFormatException exception) {
+                System.out.println("Please enter a valid input integer");
+            }
+        }
+        return output;
+    }
+
 
     /**
-    *Gets the shopping cart attached to store view
-    *@return ShoppingCart, the cart for this storeview
-    */
+     *Gets the shopping cart attached to store view
+     *@return ShoppingCart, the cart for this storeview
+     */
     public ShoppingCart getCart() {
         return cart;
     }
@@ -44,7 +64,6 @@ public class StoreView {
             System.out.println(sm.getInventory().getProductInfo(p));
         }
 
-
         boolean done = false;
         while (!done) {
             System.out.println("------------------------------------------------------");
@@ -59,32 +78,37 @@ public class StoreView {
             String storeChoice = x.nextLine();
 
             if (storeChoice.compareTo("add") == 0) {
-
+                System.out.println("------------------------------------------------------");
                 System.out.println("Enter the item ID of the item you would like to add");
-                Scanner newItem = new Scanner(System.in);
-                int itemChoice = Integer.parseInt(newItem.nextLine());
 
-                System.out.println("There are currently " + sm.getInventory().getStock(itemChoice) + " " + sm.getInventory().getProduct(itemChoice).getItemName() + " in stock.");
-                System.out.println("How many would you like to add to your cart?");
+                    int itemChoice = sv.verifyNumInput();
 
-                Scanner add = new Scanner(System.in);
-                int addItems = Integer.parseInt(add.nextLine());
+                    if (sm.getInventory().getStock(itemChoice) != -1) {
 
-                sv.getCart().addToCart(addItems, itemChoice);
+                        System.out.println("There are currently " + sm.getInventory().getStock(itemChoice) + " " + sm.getInventory().getProduct(itemChoice).getItemName() + " in stock.");
+                        System.out.println("How many would you like to add to your cart?");
+
+                        int addItems = sv.verifyNumInput();
+
+                        sv.getCart().addToCart(addItems, itemChoice);
+                    }
 
             } else if (storeChoice.compareTo("remove") == 0) {
-
+                System.out.println("------------------------------------------------------");
                 System.out.println("Enter the item ID of the item you would like to remove");
-                Scanner newItem = new Scanner(System.in);
-                int itemChoice = Integer.parseInt(newItem.nextLine());
+                int itemChoice = sv.verifyNumInput();
 
-                System.out.println("You currently have " + sv.getCart().getCartAmount(itemChoice) + " in your cart.");
-                System.out.println("How many would you like to remove?");
-                System.out.println("Please note that removing any more than you have will fully remove the item.");
+                if(sv.getCart().getCartAmount(itemChoice) != 0) {
+                    System.out.println("You currently have " + sv.getCart().getCartAmount(itemChoice) + " in your cart.");
+                    System.out.println("How many would you like to remove?");
+                    System.out.println("Please note that removing any more than you have will fully remove the item.");
 
-                Scanner remove = new Scanner(System.in);
-                int removeItems = Integer.parseInt(remove.nextLine());
-                sv.getCart().removeFromCart(removeItems, itemChoice);
+                    int removeItems = sv.verifyNumInput();
+                    sv.getCart().removeFromCart(removeItems, itemChoice);
+                }
+                else{
+                    System.out.println("There is no item(s) with such an id currently in your cart.");
+                }
 
             } else if (storeChoice.compareTo("view") == 0) {
                 System.out.println("------------------------------------------------------");
